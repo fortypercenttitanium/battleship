cbb003bf310be30cd52f784d2243c08e77c94fd2
Alex Younger <alexanderyounger@gmail.com>
2020-12-08 12:45:42 -0500

    removed errant useeffect

--- src/components/game_window/Init.js
+++ src/components/game_window/Init.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import Player from '../../factories/playerFactory';
 import {
 	InitWindow,
