d3996561e2959604c9bfbc4938b78a2a34ab9842
Alex Younger <alexanderyounger@gmail.com>
2021-06-19 19:53:02 -0400

    switch volume icons

--- src/components/game_window/GameWindow.js
+++ src/components/game_window/GameWindow.js
@@ -1,9 +1,9 @@
 import React, {
-	useState,
-	useContext,
-	useEffect,
-	useRef,
-	useCallback,
+  useState,
+  useContext,
+  useEffect,
+  useRef,
+  useCallback,
 } from 'react';
 import Init from './Init';
 import GameSetup from './GameSetup';
@@ -11,8 +11,8 @@ import GameStart from './GameStart';
 import WinnerScreen from './WinnerScreen';
 import { store } from '../../GameController';
 import {
-	MainWindow,
-	VolumeContainer,
+  MainWindow,
+  VolumeContainer,
 } from '../styled_components/gameControllerStyles';
 import music from '../../assets/sounds/music.mp3';
 import backgroundSound from '../../assets/sounds/background_sound.mp3';
@@ -23,133 +23,133 @@ import VolumeOn from '../icons/VolumeOn';
 import VolumeOff from '../icons/VolumeOff';
 
 export default function GameWindow() {
-	const { state } = useContext(store);
-	const { timeline, winner } = state;
-	const [dismount, setDismount] = useState(false);
-	const [volume, setVolume] = useState(true);
+  const { state } = useContext(store);
+  const { timeline, winner } = state;
+  const [dismount, setDismount] = useState(false);
+  const [volume, setVolume] = useState(true);
 
-	// pass to props to avoiding directly passing a setState function
-	const setVolumeProps = (value) => {
-		value
-			? (musicPlayer.current.volume = 0.5)
-			: (musicPlayer.current.volume = 0);
-		setVolume(value);
-	};
+  // pass to props to avoiding directly passing a setState function
+  const setVolumeProps = (value) => {
+    value
+      ? (musicPlayer.current.volume = 0.5)
+      : (musicPlayer.current.volume = 0);
+    setVolume(value);
+  };
 
-	const musicPlayer = useRef();
-	// I provide two sound players so sound effects can "overlap"
-	const soundPlayer = useRef();
-	const soundPlayer2 = useRef();
+  const musicPlayer = useRef();
+  // I provide two sound players so sound effects can "overlap"
+  const soundPlayer = useRef();
+  const soundPlayer2 = useRef();
 
-	// cancel animation coming into this component
-	useEffect(() => {
-		setDismount(false);
-	}, [setDismount]);
+  // cancel animation coming into this component
+  useEffect(() => {
+    setDismount(false);
+  }, [setDismount]);
 
-	// to avoid passing a setState directly, pass this helper function
-	const setDismountProp = (state) => {
-		setDismount(state);
-	};
+  // to avoid passing a setState directly, pass this helper function
+  const setDismountProp = (state) => {
+    setDismount(state);
+  };
 
-	const playBgSound = useCallback(
-		(sound, customVolume) => {
-			if (volume) {
-				const newVol = customVolume || 0.5;
-				if (!musicPlayer.current.paused) musicPlayer.current.pause();
-				musicPlayer.current.src =
-					sound === 'music'
-						? music
-						: sound === 'bgSound'
-						? backgroundSound
-						: null;
-				musicPlayer.current.load();
-				musicPlayer.current.volume = newVol;
-				musicPlayer.current.play();
-			}
-		},
-		[volume]
-	);
+  const playBgSound = useCallback(
+    (sound, customVolume) => {
+      if (volume) {
+        const newVol = customVolume || 0.5;
+        if (!musicPlayer.current.paused) musicPlayer.current.pause();
+        musicPlayer.current.src =
+          sound === 'music'
+            ? music
+            : sound === 'bgSound'
+            ? backgroundSound
+            : null;
+        musicPlayer.current.load();
+        musicPlayer.current.volume = newVol;
+        musicPlayer.current.play();
+      }
+    },
+    [volume],
+  );
 
-	const playSound = useCallback(
-		(sound, customVolume) => {
-			if (volume) {
-				const newVol = customVolume || 0.5;
-				let player = soundPlayer;
-				if (!soundPlayer.current.paused) {
-					player = soundPlayer2;
-				}
-				player.current.src =
-					sound === 'fireShot'
-						? fireShot
-						: sound === 'shotMiss'
-						? shotMiss
-						: sound === 'shotHit'
-						? shotHit
-						: null;
-				player.current.load();
-				player.current.volume = newVol;
-				player.current.play();
-			}
-		},
-		[volume]
-	);
+  const playSound = useCallback(
+    (sound, customVolume) => {
+      if (volume) {
+        const newVol = customVolume || 0.5;
+        let player = soundPlayer;
+        if (!soundPlayer.current.paused) {
+          player = soundPlayer2;
+        }
+        player.current.src =
+          sound === 'fireShot'
+            ? fireShot
+            : sound === 'shotMiss'
+            ? shotMiss
+            : sound === 'shotHit'
+            ? shotHit
+            : null;
+        player.current.load();
+        player.current.volume = newVol;
+        player.current.play();
+      }
+    },
+    [volume],
+  );
 
-	const checkIfMusicPaused = () => {
-		return musicPlayer.current.paused;
-	};
+  const checkIfMusicPaused = () => {
+    return musicPlayer.current.paused;
+  };
 
-	const fadeOutMusic = () => {
-		const fadeOut = setInterval(() => {
-			if (musicPlayer.current.volume <= 0.04) {
-				musicPlayer.current.volume = 0;
-				clearInterval(fadeOut);
-			} else {
-				musicPlayer.current.volume = musicPlayer.current.volume - 0.03;
-			}
-		}, 30);
-	};
+  const fadeOutMusic = () => {
+    const fadeOut = setInterval(() => {
+      if (musicPlayer.current.volume <= 0.04) {
+        musicPlayer.current.volume = 0;
+        clearInterval(fadeOut);
+      } else {
+        musicPlayer.current.volume = musicPlayer.current.volume - 0.03;
+      }
+    }, 30);
+  };
 
-	// conditionally render based on the app state "timeline"
-	const renderChild = (timeline) => {
-		return timeline === 'init' ? (
-			<Init
-				playBgSound={playBgSound}
-				checkIfMusicPaused={checkIfMusicPaused}
-				dismount={dismount}
-				setDismount={setDismountProp}
-			/>
-		) : timeline === 'setup' ? (
-			<GameSetup
-				dismount={dismount}
-				fadeOutMusic={fadeOutMusic}
-				setDismount={setDismountProp}
-			/>
-		) : winner ? (
-			<WinnerScreen playBgSound={playBgSound} />
-		) : !winner ? (
-			<GameStart
-				playSound={playSound}
-				playBgSound={playBgSound}
-				setDismount={setDismount}
-			/>
-		) : null;
-	};
+  // conditionally render based on the app state "timeline"
+  const renderChild = (timeline) => {
+    return timeline === 'init' ? (
+      <Init
+        playBgSound={playBgSound}
+        checkIfMusicPaused={checkIfMusicPaused}
+        dismount={dismount}
+        setDismount={setDismountProp}
+      />
+    ) : timeline === 'setup' ? (
+      <GameSetup
+        dismount={dismount}
+        fadeOutMusic={fadeOutMusic}
+        setDismount={setDismountProp}
+      />
+    ) : winner ? (
+      <WinnerScreen playBgSound={playBgSound} />
+    ) : !winner ? (
+      <GameStart
+        playSound={playSound}
+        playBgSound={playBgSound}
+        setDismount={setDismount}
+      />
+    ) : null;
+  };
 
-	return (
-		<MainWindow>
-			<VolumeContainer timeline={timeline}>
-				{volume ? (
-					<VolumeOff setVolume={setVolumeProps} />
-				) : (
-					<VolumeOn setVolume={setVolumeProps} />
-				)}
-			</VolumeContainer>
-			{renderChild(timeline)}
-			<>
-				<audio onEnded={() => musicPlayer.current.play()} ref={musicPlayer} />
-				<audio ref={soundPlayer} />
-				<audio ref={soundPlayer2} />
-			</>
-		</MainWindow>
-	);
+  return (
+    <MainWindow>
+      <VolumeContainer timeline={timeline}>
+        {volume ? (
+          <VolumeOn setVolume={setVolumeProps} />
+        ) : (
+          <VolumeOff setVolume={setVolumeProps} />
+        )}
+      </VolumeContainer>
+      {renderChild(timeline)}
+      <>
+        <audio onEnded={() => musicPlayer.current.play()} ref={musicPlayer} />
+        <audio ref={soundPlayer} />
+        <audio ref={soundPlayer2} />
+      </>
+    </MainWindow>
+  );
 }
